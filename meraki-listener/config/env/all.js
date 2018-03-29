47f3ddf8ba10d412f42d22958d721d6086d73e88
cdupland <cyril.dupland@gmail.com>
2018-03-29 14:38:53 +0200

    Add places_list configuration.
    Signed-off-by: cdupland <cyril.dupland@gmail.com>

--- meraki-listener/config/env/all.js
+++ meraki-listener/config/env/all.js
@@ -29,5 +29,6 @@ module.exports = {
         apikey: process.env.MAPWISE_APIKEY,
         apiurl: process.env.MAPWISE_APIURL
     },
-    secret_hash: process.env.SECRET_HASH
+    secret_hash: process.env.SECRET_HASH,
+    places_list: process.env.PLACES_LIST
 };
