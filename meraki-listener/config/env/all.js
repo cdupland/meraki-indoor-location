4dc537b2d684fd0e3508b54b8ba740228c4d2a10
cdupland <cyril.dupland@gmail.com>
2018-04-20 10:35:35 +0200

    Config.
    Signed-off-by: cdupland <cyril.dupland@gmail.com>

--- meraki-listener/config/env/all.js
+++ meraki-listener/config/env/all.js
@@ -2,33 +2,33 @@
 
 module.exports = {
     port: process.env.PORT || 3004,
-    secret: '8985058',
-    validator: 'c1fd91ca5c98a45920d34dec9fa6c3462e0f8b17',
+    secret: process.env.SECRET,
+    validator: process.env.VALIDATOR,
     // See all valid formats in: https://www.npmjs.com/package/bytes
     maxBodySize: process.env.MAX_BODY_SIZE || '50mb',
-    floorPlans: process.env.FLOOR_PLANS ? JSON.parse(process.env.FLOOR_PLANS) : [{"name":"Terminal Billi","floor":null,"merakiCorners":[{"lng":-0.7007566259670723,"lat":44.8295250335519},{"lng":-0.6989086056,"lat":44.829533047},{"lng":-0.7007512832,"lat":44.8289053035},{"lng":-0.6989032628329279,"lat":44.82891331703426}],"mapwizeCorners":[{"lat":44.8295250335519,"lng":-0.7007566259670723},{"lat":44.829533047,"lng":-0.6989086056},{"lat":44.8289053035,"lng":-0.7007512832},{"lat":44.82891331703426,"lng":-0.6989032628329279}]}],
-    macAddressEnabled: true,
+    floorPlans: process.env.FLOOR_PLANS ? JSON.parse(process.env.FLOOR_PLANS) : [],
+    macAddressEnabled: process.env.MAC_ADDRESS_ENABLED || false,
     redis: {
-        host: 'redisAirportBOD.redis.cache.windows.net',
+        host: process.env.REDIS_HOST,
         port: process.env.REDIS_PORT || 6379,
-        password: 'whP2NfpNI0RCmF9N5Ou1RUPGPkfvO0aWO/P+Mkob+p0=',
+        password: process.env.REDIS_AUTH,
         merakiNotificationTTL: process.env.REDIS_MERAKI_NOTIF_TTL || 3600
     },
     documentDB: {
-        enabled: true,
-        endpoint: 'https://data-bziiit.documents.azure.com:443/',
-        primaryKey: 'cf6UwuAFV6WfJVQSmTIPmBchGma2w6tzjIE09Hlln0BeUtXSTX1sZnk388wavJc95WumKnF0NZV1oHOZC4MguA==',
-        database: 'shared-data-bziiit',
-        collection: 'iot_meraki_bod'
+        enabled: process.env.DOCUMENT_DB_ENABLED,
+        endpoint: process.env.DOCUMENT_DB_ENDPOINT,
+        primaryKey: process.env.DOCUMENT_DB_PRIMARY_KEY,
+        database: process.env.DOCUMENT_DB_DATABASE,
+        collection: process.env.DOCUMENT_DB_COLLECTION
     },
     mapwise: {
-        user: 'cd@bziiit.com',
-        password: 'bziiit2018',
-        venueid: '5abd11f1bd863f0013eaffd2',
-        organizationid: '5a68b8071a87c000133152a1',
-        apikey: '0f77d2bcde1ca3d6707b86fe24c2b74b',
-        apiurl: 'https://api.mapwize.io'
+        user: process.env.MAPWISE_USER,
+        password: process.env.MAPWISE_PASSWORD,
+        venueid: process.env.MAPWISE_VENUEID,
+        organizationid: process.env.MAPWISE_ORGANIZATIONID,
+        apikey: process.env.MAPWISE_APIKEY,
+        apiurl: process.env.MAPWISE_APIURL
     },
-    secret_hash: 'bodBZport',
-    places_list: []
+    secret_hash: process.env.SECRET_HASH,
+    places_list: process.env.PLACES_LIST
 };
