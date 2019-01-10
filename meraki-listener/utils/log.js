5438a84d8894929573ce1162205d730546728e0e
cdupland <cyril.dupland@gmail.com>
2019-01-10 15:55:46 +0100

    CHange system log with simple file.

--- meraki-listener/utils/log.js
+++ meraki-listener/utils/log.js
@@ -1,4 +1,9 @@
 var winston = require('winston');
+var os = require('os');
+var moment = require('moment-timezone');
+var config = require('../config/config');
+
+const fs = require('fs');
 const log = winston.createLogger({
     // levels: winston.config.syslog.levels,
     // level: 'info',
@@ -10,11 +15,36 @@ const log = winston.createLogger({
     ]
   });
 
+const log_folder = './log/' ;
+const error_file = 'error.log' ;
+const log_file = 'combined.log' ;
 
-exports.log = function(msg) {
-    log.info(msg);
+const date_folder_format = 'YYYY-MM-DD_HH:00:00' ;
+const date_line_format = 'YYYY-MM-DD HH:mm:ss' ;
+
+exports.log = function(msg){
+    writeFile(log_file,msg);
 };
 
-exports.error = function(msg) {
-    log.error(msg);
+exports.error = function(msg){
+    writeFile(error_file,msg);
 };
+
+function writeFile(file,txt){
+    file = log_folder + /* moment().tz(config.timezone).format(date_folder_format) + '_' + */ file ;
+    txt = moment().tz(config.timezone).format(date_line_format) + " " + txt + os.EOL ;
+
+    fs.appendFile(file, txt, function(err) {
+        if(err) {
+            return console.log(err);
+        }    
+    });
+}
+
+// exports.log = function(msg) {
+//     log.info(msg);
+// };
+
+// exports.error = function(msg) {
+//     log.error(msg);
+// };
