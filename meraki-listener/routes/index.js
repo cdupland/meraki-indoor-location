cfd5c6f8c009dc860e65f09d5ab4774682c8d1dd
cdupland <cyril.dupland@gmail.com>
2019-02-27 18:33:19 +0100

    Re active event hub.

--- meraki-listener/routes/index.js
+++ meraki-listener/routes/index.js
@@ -51,14 +51,14 @@ exports.processMerakiNotifications = function (req, res) {
             }
 
             // Do whatever you want with the observations received here
-            // eventHub.sendMessage({
-            //     indoorLocation: indoorLocation,
-            //     merakiObservation: globalObservation,
-            //     merakiObservation_latitude : globalObservation.location.lat,
-            //     merakiObservation_longitude : globalObservation.location.lng,
-            //     merakiObservation_unc : globalObservation.location.unc,
-            //     secret: config.secret
-            // });
+            eventHub.sendMessage({
+                indoorLocation: indoorLocation,
+                merakiObservation: globalObservation,
+                merakiObservation_latitude : globalObservation.location.lat,
+                merakiObservation_longitude : globalObservation.location.lng,
+                merakiObservation_unc : globalObservation.location.unc,
+                secret: config.secret
+            });
 
         });
 
